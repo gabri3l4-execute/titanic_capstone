86175ad88de0708936b379b2567dc3d51d16fe11
Siying Rong <blair.rongsiying@163.com>
2026-02-09 15:34:58 +0100

    code update

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, PredictionFormView, PredictionListView
+from .views import PredictionFormView 
 
 urlpatterns = [
     path('predict/', PredictionFormView.as_view(), name='predict'),
